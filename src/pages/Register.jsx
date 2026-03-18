c045aa73daa5b73397bb30849ee4b40c2cdbeb16
Jhefferson <jheffersonjhgg@gmail.com>
2026-03-17 22:15:50 -0500

    registro

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,68 @@
 import { useState } from "react";
 import { RegisterFirstStep } from "../components/register/FirstStep";
 import { RegisterSecondStep } from "../components/register/SecondStep";
+import { RegisterThirdStep } from "../components/register/ThirdStep";
 
 export function Register() {
 
     const [step, setStep] = useState(1);
+    const [isCompleteStepOne, setIsCompleteStepOne] = useState(false);
+    const [isCompleteStepTwo, setIsCompleteStepTwo] = useState(false);
+    const [formRegister, setFormRegister] = useState({
+        email: "",
+        password: "",
+        name: "",
+        lastName: "",
+        birthDate: "",
+        dni: "",
+        phone: "",
+        gender: "",
+    })
+
+    const handleSecondStep = (email, password) => {
+        setFormRegister({
+            email,
+            password,
+        })
+        setIsCompleteStepOne(true);
+        setStep(2);
+    }
+
+    const handleFirstStep = () => {
+      setIsCompleteStepTwo(false);
+      setStep(1);
+    }
+
+    const handleBackToSecondStep = () => {
+      setIsCompleteStepTwo(false);
+      setStep(2);
+    }
+
+    const handleThirdStep = (name, lastName, birthDate, dni, phone, gender) => {
+      setFormRegister({
+        ...formRegister,
+        name,
+        lastName,
+        birthDate,
+        dni,
+        phone,
+        gender
+      });
+      setIsCompleteStepTwo(true);
+      setStep(3);
+    };
+
+    const handleRegister = (petName, petBirthDate, petType, petBreed, petGender) => {
+      const finalForm = {
+        ...formRegister,
+        petName,
+        petBirthDate,
+        petType,  
+        petBreed,
+        petGender
+      }
+      console.log(finalForm);
+    }
 
   return (
     <div className="flex flex-col items-center justify-center">
@@ -13,20 +71,20 @@ export function Register() {
         Datos de la sesión
       </h1>
       <div className="flex items-center justify-center gap-7.5 mt-11 mb-7.5">
-        <div className={(step === 1 ? "bg-[#32ACDC] text-white" : "text-[#A8AFB9] bg-white") + " rounded-full w-12.5 h-12.5 flex items-center justify-center"}>1</div>
-        <div className={(step === 2 ? "bg-[#32ACDC] text-white" : "text-[#A8AFB9] bg-white") + " rounded-full w-12.5 h-12.5 flex items-center justify-center"}>2</div>
+        <div className={(step === 1 ? "bg-[#32ACDC] text-white" : isCompleteStepOne ? "text-white bg-[#96C268]" : "text-[#A8AFB9] bg-white") + " rounded-full w-12.5 h-12.5 flex items-center justify-center"}>1</div>
+        <div className={(step === 2 ? "bg-[#32ACDC] text-white" : isCompleteStepTwo ? "text-white bg-[#96C268]" : "text-[#A8AFB9] bg-white") + " rounded-full w-12.5 h-12.5 flex items-center justify-center"}>2</div>
         <div className={(step === 3 ? "bg-[#32ACDC] text-white" : "text-[#A8AFB9] bg-white") + " rounded-full w-12.5 h-12.5 flex items-center justify-center"}>3</div>
       </div>
       <form className="m-3.5">
         {
-            step === 1 ? <RegisterFirstStep /> : step === 2 ? <RegisterSecondStep /> : null
+            step === 1 ? <RegisterFirstStep handleSecondStep={handleSecondStep} /> : step === 2 ? <RegisterSecondStep handlerFirstStep={handleFirstStep} handleThirdStep={handleThirdStep} /> : <RegisterThirdStep handlerSecondStep={handleBackToSecondStep} handleRegister={handleRegister} />
         }
       </form>
-      <div>
+      <div className="mb-10">
         <span className="text-[#A8AFB9] font-semibold">
           ¿No tienes una cuenta?{" "}
-          <a href="/register" className="text-[#F29455]">
-            Regístrate
+          <a href="/login" className="text-[#F29455] underline">
+            Iniciar Sesión
           </a>
         </span>
       </div>
